bb5945808eef3607ad75c3338b8da2047cb5408d
SRINU <vasu_anand01@yahoo.co.in>
2024-01-07 12:52:44 +0530

    1.0.1

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "test" {
-    source ="git::https://github.com/SRINU42/tf-app-module.git"
+#    source = "git::https://github.com/SRINU42/tf-app-module.git"
+    source = "github.com/SRINU42/tf-app-module.git"
     env = dev
 }
