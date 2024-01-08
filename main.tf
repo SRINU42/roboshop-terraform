9e76073257b2028c77e3f5f1321f1208ab6b87d1
SRINU <vasu_anand01@yahoo.co.in>
2024-01-08 10:12:38 +0530

    1

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-module "test" {
+#module "test" {
 #    source = "git::https://github.com/SRINU42/tf-app-module.git"
-    source = "github.com/SRINU42/tf-app-module.git"
-    env = dev
+#    source = "github.com/SRINU42/tf-app-module.git"
+#    env = dev
+#}
+
+module "test" {
+  source = "github.com/SRINU42/tf-module-app.git"
+  env = dev
 }
